925b49644fc78c2d176c2052d82a0751f1c1711b
wmik <williemik.wmik@gmail.com>
2020-01-11 19:21:40 +0300

    feat: theming
    - Setup styled components
    - Add alternate themes (dark,light)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React from "react";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 
 const MINUTES = 60;
 const HOURS = MINUTES * 60;
 
-export default function App() {
+const darkTheme = {
+  color: "hsl(210, 50%, 20%)",
+  bgColor: "hsl(210, 20%, 45%)"
+};
+
+const lightTheme = {
+  color: "hsl(210, 30%, 40%)",
+  bgColor: "hsl(210, 20%, 95%)"
+};
+
+const GlobalStyle = createGlobalStyle`
+body {
+   background-color: ${props => props.theme.bgColor};
+   transition: all 0.3s ease-in-out;
+}
+`;
+
+const TimerDisplay = styled.section`
+  text-align: center;
+  font-family: monospace;
+  color: ${props => props.theme.color};
+  border: 4px solid ${props => props.theme.color};
+  border-radius: 50%;
+  height: 320px;
+  width: 320px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+function TimerApp({ lightMode, setLightMode }) {
   const [active, setActive] = React.useState(false);
   const [seconds, setSeconds] = React.useState(0);
   const renderSeconds = (seconds % MINUTES).toLocaleString("en-US", {
@@ -29,21 +61,7 @@ export default function App() {
   });
   return (
     <article>
-      <section
-        style={{
-          textAlign: "center",
-          fontFamily: "monospace",
-          color: "#333",
-          border: "1px solid #333",
-          borderRadius: "50%",
-          height: 320,
-          width: 320,
-          margin: "0 auto",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center"
-        }}
-      >
+      <TimerDisplay>
         <header>
           <h1 style={{ fontSize: "4em" }}>
             {renderHours}:{renderMinutes}:{renderSeconds}
@@ -52,13 +70,26 @@ export default function App() {
         <footer>
           <p style={{ fontSize: "2em", margin: 0 }}>HH:MM:SS</p>
         </footer>
-      </section>
+      </TimerDisplay>
       <button type="button" onClick={() => setActive(!active)}>
         {active ? "pause" : "start"}
       </button>
       <button type="reset" onClick={() => setSeconds(0)}>
         reset
       </button>
+      <button type="button" onClick={() => setLightMode(!lightMode)}>
+        {lightMode ? "dark" : "light"} theme
+      </button>
     </article>
+  );
+}
+
+export default function App() {
+  const [lightMode, setLightMode] = React.useState(true);
+  return (
+    <ThemeProvider theme={lightMode ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      <TimerApp setLightMode={setLightMode} lightMode={lightMode} />
+    </ThemeProvider>
   );
 }

e5e742feaa104e200944e0207d383b4911baa502
wmik <williemik.wmik@gmail.com>
2020-01-12 07:43:32 +0300

    test: app
    - Setup test dependencies
    - Setup initial tests
    - Rename project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import React from 'react';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 const MINUTES = 60;
 const HOURS = MINUTES * 60;
 
-const darkTheme = {
-  color: "hsl(210, 50%, 20%)",
-  bgColor: "hsl(210, 20%, 45%)"
+export const darkTheme = {
+  color: 'hsl(210, 50%, 20%)',
+  bgColor: 'hsl(210, 20%, 45%)'
 };
 
-const lightTheme = {
-  color: "hsl(210, 30%, 40%)",
-  bgColor: "hsl(210, 20%, 95%)"
+export const lightTheme = {
+  color: 'hsl(210, 30%, 40%)',
+  bgColor: 'hsl(210, 20%, 95%)'
 };
 
 const GlobalStyle = createGlobalStyle`
@@ -35,21 +35,21 @@ const TimerDisplay = styled.section`
   justify-content: center;
 `;
 
-function TimerApp({ lightMode, setLightMode }) {
+export function TimerApp({ lightMode, setLightMode }) {
   const [active, setActive] = React.useState(false);
   const [seconds, setSeconds] = React.useState(0);
-  const renderSeconds = (seconds % MINUTES).toLocaleString("en-US", {
+  const renderSeconds = (seconds % MINUTES).toLocaleString('en-US', {
     minimumIntegerDigits: 2,
     useGrouping: false
   });
   const renderMinutes = Math.floor((seconds % HOURS) / MINUTES).toLocaleString(
-    "en-US",
+    'en-US',
     {
       minimumIntegerDigits: 2,
       useGrouping: false
     }
   );
-  const renderHours = Math.floor(seconds / HOURS).toLocaleString("en-US", {
+  const renderHours = Math.floor(seconds / HOURS).toLocaleString('en-US', {
     minimumIntegerDigits: 2,
     useGrouping: false
   });
@@ -66,22 +66,22 @@ function TimerApp({ lightMode, setLightMode }) {
     <article>
       <TimerDisplay>
         <header>
-          <h1 style={{ fontSize: "4em" }}>
+          <h1 style={{ fontSize: '4em' }}>
             {renderHours}:{renderMinutes}:{renderSeconds}
           </h1>
         </header>
         <footer>
-          <p style={{ fontSize: "2em", margin: 0 }}>HH:MM:SS</p>
+          <p style={{ fontSize: '2em', margin: 0 }}>HH:MM:SS</p>
         </footer>
       </TimerDisplay>
       <button type="button" onClick={() => setActive(!active)}>
-        {active ? "pause" : "start"}
+        {active ? 'pause' : 'start'}
       </button>
       <button type="reset" onClick={() => setSeconds(0)}>
         reset
       </button>
       <button type="button" onClick={() => setLightMode(!lightMode)}>
-        {lightMode ? "dark" : "light"} theme
+        {lightMode ? 'dark' : 'light'} theme
       </button>
     </article>
   );
---
+++ src/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { TimerApp, lightTheme } from './App';
+
+test('TimerApp renders without crashing', async () => {
+  const setLightMode = jest.fn();
+  const { findByText } = render(
+    <ThemeProvider theme={lightTheme}>
+      <TimerApp lightMode={true} setLightMode={setLightMode} />
+    </ThemeProvider>
+  );
+  expect(await findByText(/00:00:00/, { selector: 'h1' })).toBeVisible();
+  expect(await findByText(/HH:MM:SS/, { selector: 'p' })).toBeVisible();
+  expect(await findByText(/start/, { selector: 'button' })).toBeVisible();
+  expect(await findByText(/reset/, { selector: 'button' })).toBeVisible();
+  expect(await findByText(/dark theme/, { selector: 'button' })).toBeVisible();
+});
